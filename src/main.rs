391facfa7845aa850375b50812574dcd9d83d7ff
Jeremy Cardona <jeremy.cardona@hotmail.com>
2026-02-05 15:18:07 -0400

    succesfully load data from csv file, prints data to console.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
-use chrono:: prelude::*;
-use polars_core::prelude::DataType;
 use polars::prelude::*;
 
 fn load_csv(csv_file: &str) -> LazyFrame {
-    let lazy_frame = LazyCsvReader::new(csv_file)
-        .has_header(true)
-        .finish()?;
+    let reader_lazy_frame = LazyCsvReader::new(PlPath::new(csv_file))
+        .with_has_header(true)
+        .finish();
 
-    match lazy_frame {
-        Ok(lf) => {
+    match reader_lazy_frame{
+        Ok(data) => {
             println!("Successfully loaded: {}", csv_file);
             
             // returns lazy data frame.
-            lf
+            data 
         },
         Err(e) => {
             eprintln!("Error: Could not open the file found in {}! Details: {},", csv_file, e);
@@ -24,24 +22,11 @@ fn load_csv(csv_file: &str) -> LazyFrame {
     
 }
 
-fn example() {
-    let mut df: DataFrame = df!(
-    "name" => ["Alice Archer", "Ben Brown", "Chloe Cooper", "Daniel Donovan"],
-    "birthdate" => [
-        NaiveDate::from_ymd_opt(1997, 1, 10).unwrap(),
-        NaiveDate::from_ymd_opt(1985, 2, 15).unwrap(),
-        NaiveDate::from_ymd_opt(1983, 3, 22).unwrap(),
-        NaiveDate::from_ymd_opt(1981, 4, 30).unwrap(),
-    ],
-    "weight" => [57.9, 72.5, 53.6, 83.1],  // (kg)
-    "height" => [1.56, 1.77, 1.65, 1.75],  // (m)
-    )
-    .unwrap();
-    println!("{df}");
-}
 fn main() {
 
     // lazy data frame to manipulate
-    let data = load_csv("data.csv");
-    println!("{}", data);
+    let frame = load_csv("data.csv");
+    let data_frame = frame.collect().expect("Failed to process data");
+
+    println!("{}", data_frame);
 }
