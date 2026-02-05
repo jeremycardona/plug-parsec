bbb8f8f9ee03781e30f82d0cc861e8acfda12644
Jeremy Cardona <jeremy.cardona@hotmail.com>
2026-02-05 14:11:48 -0400

    at this point im getting a bunch of compiler errors.

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# Plug Parser - READ ME FIRST!
+# Plug Parser - READ ME FIRST
+
 Demonstration of how to write a parser with Rust.
 The parser combinator library that will be used will be **nom**.
 
-
 ## Dependencies
-Pola.rs - for data manipulation.
-idk if i really need nom for a parser cqombinator.
+
+### Pola.rs - for data manipulation
+
+Features used: lazy.
+
+### idk if i really need nom for a parser cqombinator
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use chrono:: prelude::*;
+use polars_core::prelude::DataType;
+use polars::prelude::*;
+
+fn load_csv(csv_file: &str) -> LazyFrame {
+    let lazy_frame = LazyCsvReader::new(csv_file)
+        .has_header(true)
+        .finish()?;
+
+    match lazy_frame {
+        Ok(lf) => {
+            println!("Successfully loaded: {}", csv_file);
+            
+            // returns lazy data frame.
+            lf
+        },
+        Err(e) => {
+            eprintln!("Error: Could not open the file found in {}! Details: {},", csv_file, e);
+            
+            // returns empty lazy data frame.
+            DataFrame::empty().lazy()
+        }
+    }
+    
+}
+
+fn example() {
+    let mut df: DataFrame = df!(
+    "name" => ["Alice Archer", "Ben Brown", "Chloe Cooper", "Daniel Donovan"],
+    "birthdate" => [
+        NaiveDate::from_ymd_opt(1997, 1, 10).unwrap(),
+        NaiveDate::from_ymd_opt(1985, 2, 15).unwrap(),
+        NaiveDate::from_ymd_opt(1983, 3, 22).unwrap(),
+        NaiveDate::from_ymd_opt(1981, 4, 30).unwrap(),
+    ],
+    "weight" => [57.9, 72.5, 53.6, 83.1],  // (kg)
+    "height" => [1.56, 1.77, 1.65, 1.75],  // (m)
+    )
+    .unwrap();
+    println!("{df}");
+}
 fn main() {
-    println!("Hello world.");
+
+    // lazy data frame to manipulate
+    let data = load_csv("data.csv");
+    println!("{}", data);
 }
